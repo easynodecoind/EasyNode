bd00af6ad9b8d4e9daeb274c6ec505d01cebcbc8
EasyCoin Developer <easynode@tuta.io>
2018-04-09 00:53:38 +0300

    Change ports

--- src/chainparamsbase.cpp
+++ src/chainparamsbase.cpp
@@ -32,7 +32,7 @@ class CBaseMainParams : public CBaseChainParams
 public:
     CBaseMainParams()
     {
-        nRPCPort = 12454;
+        nRPCPort = 12450;
     }
 };
 static CBaseMainParams mainParams;
@@ -45,7 +45,7 @@ class CBaseTestNetParams : public CBaseChainParams
 public:
     CBaseTestNetParams()
     {
-        nRPCPort = 13454;
+        nRPCPort = 13450;
         strDataDir = "testnet3";
     }
 };
@@ -59,7 +59,7 @@ class CBaseRegTestParams : public CBaseChainParams
 public:
     CBaseRegTestParams()
     {
-        nRPCPort = 13564;
+        nRPCPort = 13560;
         strDataDir = "regtest";
     }
 };
